fc13c8dccb107407bfdae001d05975d9055c6b6a
Samuel McLaughlin <mclaughlinsamuel9@gmail.com>
2025-12-02 15:36:20 +0100

    Add SQLite practice queries

---
+++ sqlite/queries/customers.sql
@@ -0,0 +1,14 @@
+/*
+
+Created by: Samuel McLaughlin
+Create date: 2025/12/01
+Description: This query displays all customers first, last, and email addresses. 
+
+*/
+
+SELECT
+	FirstName,
+	LastName,
+	Email
+FROM
+	Customer
